708810652e3de64a65c9cc0c39ebc7ad44dba110
Coder-Serwin <roughuseforaronserwin@gmail.com>
2022-02-24 19:52:35 +0530

    new commit

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,5 @@
-console.log("hii")
+const input = document.getElementById("Textbtn");
+
+function Press () {
+    document.getElementById("output").textContent = document.getElementById("Textbtn").value;
+}
