cdaf069f228ed9d80c66bfcc13faedc7bf4ed1a5
Coder-Serwin <roughuseforaronserwin@gmail.com>
2022-02-25 20:05:07 +0530

    new commit

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,32 @@
-document.getElementById("header").textContent.textContent = "Hello world";
+const inputbox = document.getElementById("inputbox");
+const insertbtn = document.getElementById("insertbtn");
+const ouputbox = document.getElementById("Output");
+
+insertbtn.onclick = function () {
+    const value = inputbox.value;
+    let getLocalStorage = localStorage.getItem("New Todo");
+    if (getLocalStorage==null) {
+        listArr = [];
+    }
+    else {
+        listArr = JSON.parse(getLocalStorage);
+    }
+    listArr.push(value);
+    localStorage.setItem("New Todo",JSON.stringify(listArr));
+    showoutput();
+}
+
+function showoutput () {
+    let getLocalStorage = localStorage.getItem("New Todo");
+    if (getLocalStorage==null) {
+        listArr = [];
+    }
+    else {
+        listArr = JSON.parse(getLocalStorage);
+    }
+    let newTodo = ""
+    listArr.forEach((element,index) => {
+        newTodo = `${element}`
+    })
+    ouputbox.textContent += newTodo;
+}
