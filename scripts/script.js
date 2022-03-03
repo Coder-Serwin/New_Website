87016ac767775bba7f57d72078f2fc6b7b638e3d
Coder-Serwin <roughuseforaronserwin@gmail.com>
2022-03-03 20:04:48 +0530

    new changes

--- scripts/script.js
+++ scripts/script.js
@@ -1,32 +1,15 @@
 const speechBtn = document.querySelector(".texttospeechbBtn"),
 inputText = document.querySelector(".quote"),
 copyBtn = document.querySelector(".copy"),
-voiceList = document.querySelector("select"),
 credit = document.querySelector(".link"),
 synth = speechSynthesis;
 let isSpeaking = true;
-
-Voices();
-
-function Voices () {
-    for (let voice of synth.getVoices()) {
-        let option1 = `<option value="${voice.name}">${voice.name} (${voice.lang})</option>`;
-        voiceList.insertAdjacentHTML("beforeend",option1);
-    }
-}
-
-synth.addEventListener("voiceschanged",Voices)
 
 speechBtn.addEventListener("click", ()=>{
     if (inputText.value !== "") {
         if (!synth.speaking) {
             let utterance = new SpeechSynthesisUtterance(`${inputText.value}`);
-            for(let voices of synth.getVoices()) {
-                if (voices.name === voiceList.value) {
-                    utterance.voice = voices;
-                    synth.speak(utterance);
-                };
-            };
+            synth.speak(utterance);
     };
         if (inputText.value.length > 80 ) {
             if (isSpeaking) {
